5fba3eba471c51d9f598caee0fea5bb715ab4780
NAKULAN14 <nakulan1412@gmail.com>
2026-07-03 20:37:42 +0530

    final

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BeaconDashboard from "./BeaconDashboard";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BeaconDashboard />
+  </React.StrictMode>
+);
